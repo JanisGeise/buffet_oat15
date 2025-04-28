5f0607a1c4721213b347327d215a911796505dc3
Janis Geise <janis.geise@tu-dresden.de>
2025-04-28 12:52:51 +0200

    preliminary setup of simulation

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# buffet_oat15
-OpenFOAM simulations of transonic shock buffet
+# Transonic shock buffet of an OAT15 airfoil in OpenFOAM
+Setup for execution and analysis of simulations of transonic shock buffet for an OAT 15 airfoil
+
+## TODO
+
+- verify setup against reference data
+- influence spanwise extrusion
+- grid convergence study
+- extend post-processing scripts for analysis of cp-distributions etc.
+- refactoring and documentation
+
+
+- include sweep angle in IC
+- analyse influnce sweep angle
+- include pitching for DMDc analysis
+
+## Setup
+
+- cylinder geometry required for meshing can be created using the *create_cylinder_stl_file.py*
+- the STL file for the OAT15 airfoil has to be located in the same directory (*geometry*) in the
+top-level of the repository
+
+- the simulation setup is located in the directory `OAT15`
+- to execute the meshing run the `Allrun.pre`
+- to execute the simulation (including the meshing) run the `Allrun` script
+
+## Visualization of the results
+
+- the directory `post_pocessing` contains the scripts for post-processing and visualization of the results
+  (currently only analysis of the force coefficients)
+
+## References
+
+- J. Kleinert, M. Ehrle, A. Waldmann, and T. Lutz. Wake Tail Plane Interactions for a Tandem Wing Config-
+uration in High-Speed Stall Conditions. 2023. doi: 10.1007/s13272-023-00670-1. 
+- J. Kleinert, J. Stober, and T. Lutz. “Numerical simulation of wake interactions on a tandem wing configuration
+in high-speed stall conditions”. In: CEAS Aeronautical Journal 14.1 (2023), pp. 171–186. doi: 10 . 1007 /
+s13272-022-00634-x.
---
+++ post_processing/utils.py
@@ -0,0 +1,105 @@
+"""
+    helper functions
+"""
+import numpy as np
+import pandas as pd
+
+from glob import glob
+from os.path import join
+from scipy.signal import welch
+from typing import Union, Tuple
+from scipy.interpolate import interp1d
+
+
+def load_ratio_rans_les(path, usecols=[0, 1, 2], names=["t", "les", "rans"]) -> pd.DataFrame:
+    dirs = sorted(glob(join(path, "postProcessing", "DESField", "*")), key=lambda x: float(x.split("/")[-1]))
+    _ratio = [pd.read_csv(join(p, "DESModelRegions.dat"), sep=r"\s+", comment="#", header=None, usecols=usecols,
+                          names=names) for p in dirs]
+
+    if len(_ratio) == 1:
+        return _ratio[0]
+    else:
+        _ratio = pd.concat(_ratio)
+
+        # remove duplicates (resulting from dt < write precision) and reset the idx
+        _ratio.drop_duplicates(["t"], inplace=True)
+        _ratio.reset_index(inplace=True, drop=True)
+        return _ratio
+
+
+def load_yplus(path, usecols=[0, 2, 3, 4], names=["t", "yPlus_min", "yPlus_max", "yPlus_avg"]) -> pd.DataFrame:
+    dirs = sorted(glob(join(path, "postProcessing", "yPlus", "*")), key=lambda x: float(x.split("/")[-1]))
+    _yplus = [pd.read_csv(join(p, "yPlus.dat"), sep=r"\s+", comment="#", header=None, usecols=usecols, names=names)
+              for p in dirs]
+    if len(_yplus) == 1:
+        return _yplus[0]
+    else:
+        _yplus = pd.concat(_yplus)
+
+        # remove duplicates (resulting from dt < write precision) and reset the idx
+        _yplus.drop_duplicates(["t"], inplace=True)
+        _yplus.reset_index(inplace=True, drop=True)
+        return _yplus
+
+
+def load_force_coeffs(path, usecols=[0, 1, 4], names=["t", "cx", "cy"]) -> pd.DataFrame:
+    dirs = sorted(glob(join(path, "postProcessing", "forces", "*")), key=lambda x: float(x.split("/")[-1]))
+    coeffs = [pd.read_csv(join(p, "coefficient.dat"), sep=r"\s+", comment="#", header=None, usecols=usecols, names=names)
+              for p in dirs]
+    if len(coeffs) == 1:
+        return coeffs[0]
+    else:
+        coeffs = pd.concat(coeffs)
+
+        # remove duplicates (resulting from dt < write precision) and reset the idx
+        coeffs.drop_duplicates(["t"], inplace=True)
+        coeffs.reset_index(inplace=True, drop=True)
+        return coeffs
+
+
+def compute_fft(data: np.ndarray, dt: Union[float, int]) -> Tuple[np.ndarray, np.ndarray]:
+    _f, _a = welch(data, 1/dt, nperseg=len(data), nfft=len(data), window="boxcar")
+    return _f, _a
+
+
+def interpolate_uniform(t: np.ndarray, data: np.ndarray):
+    # get start and end time
+    t_start, t_end = t[0], t[-1]
+
+    # use standard interpolation to get values at const. dt
+    _interpolator = interp1d(t, data, fill_value="extrapolate")
+    dt = float("{:.1e}".format(t[-1] - t[-2]))
+
+    t_new = np.arange(start=t_start + dt, stop=t_end, step=dt)
+    return t_new, _interpolator(t_new)
+
+
+def get_pimple_iterations(load_path: str) -> Tuple[list, list]:
+    """
+    gets the number of PIMPLE iterations (p-U-couplings)
+
+    :param load_path: path to the top-level directory of the simulation containing the log file from the flow solver
+    :return: dict containing the mean and max. Courant numbers, and if present the mean and max. CFL from the interface
+    """
+    pattern = [r"PIMPLE: not converged within ", r"PIMPLE: converged in "]
+
+    # check if we have multiple log files, if so sort them
+    try:
+        logs = sorted(glob(join(load_path, f"log.rhoPimpleFoam*")), key=lambda x: int(x.split("_")[-1]))
+    except ValueError:
+        logs = glob(join(load_path, f"log.rhoPimpleFoam*"))
+
+    data, times = [], []
+    for log in logs:
+        with open(log, "r") as f:
+            logfile = f.readlines()
+
+        for line in logfile:
+            if line.startswith(pattern[0]) or line.startswith(pattern[1]):
+                data.append(int(line.split(" ")[-2]))
+            elif line.startswith("Time = "):
+                times.append(float(line.split()[-1].strip()))
+    return times, data
+
+if __name__ == "__main__":
+    pass
