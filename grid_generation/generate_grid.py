3bd8eade328b861e4a7a2e2244b38e8c809cf661
Janis Geise <janis.geise@tu-dresden.de>
2026-03-11 14:48:33 +0100

    refactoring & add animation script

--- grid_generation/generate_grid.py
+++ grid_generation/generate_grid.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     file_name = "oat15.dat"
 
     # path to where the blockMeshDict should be written to
-    write_path = r"../OAT15_simulations/DDES_SA_SALSA_validation_Re3e6_Ma0.73/system"
+    write_path = r"../OAT15_simulations/DDES_SA_validation_Re3e6_Ma0.73/system"
 
     # execute the blockMeshDict generation with the chosen settings
-    # for URANS: n_cells_z=1, y_min=-0.0375, for DDES: n_cells_z=30, y_min=-0.25
-    grid_generator = BlockMeshGenerator(".", file_name, write_path, reverse=True, n_cells_z=65, y_min=-0.25)
+    # for URANS: n_cells_z=1, y_min=-0.0375, for DDES: n_cells_z=30, y_min=-0.25, expansion_rate=1.2
+    grid_generator = BlockMeshGenerator(".", file_name, write_path, reverse=True, n_cells_z=65, y_min=-0.25,
+                                        expansion_rate=1.1)
     grid_generator.generate_block_mesh()
     grid_generator.write_block_mesh_dict()
