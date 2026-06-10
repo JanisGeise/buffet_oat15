d3c303dac9a727d86abe70c2e94c7875f9c0612d
Janis Geise <janis.geise@tu-dresden.de>
2026-06-10 14:12:18 +0200

    add arrow for inflow vector in pitching animation

--- post_processing/create_animation.py
+++ post_processing/create_animation.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 from os import makedirs
 from pandas import read_csv
 from os.path import join, exists
-from matplotlib.patches import Polygon
+from matplotlib.patches import Polygon, FancyArrowPatch
 from flowtorch.data import FOAMDataloader, mask_box
 from matplotlib.animation import FuncAnimation, FFMpegWriter
-from torchgen import yaml_utils
 
 from utils import load_force_coeffs
 
@@ -52,49 +51,64 @@ def prepare_data(load_path: str, bounds : list, save_path, case: str, field_name
     # take all available write times except zero
     _write_times = [t for t in loader.write_times if float(t) >= t_start]
 
-    # allocate a tensor for the flow fields
+    # allocate a tensor for the flow fields, for the animation we don't care about DP
     if n_dims == 2:
-        _data = pt.zeros((_coord[mask].shape[0], len(_write_times)))
+        _data = pt.zeros((_coord[mask].shape[0], len(_write_times))).to(pt.float32)
     else:
-        _data = pt.zeros((_coord.shape[0], len(_write_times)))
+        _data = pt.zeros((_coord.shape[0], len(_write_times))).to(pt.float32)
 
     # load the data, loop over times due to memory constraints for the DDES case
     print(f"Loading snapshots for field {field_name}. Found {len(_write_times)} snapshots.")
     for i, t in enumerate(_write_times):
-        _data[:, i] = pt.masked_select(loader.load_snapshot(field_name, t), mask)[_idx]
+        print(f"\rLoading write time {i + 1} / {len(_write_times)}.", end="", flush=True)
+        _data[:, i] = pt.masked_select(loader.load_snapshot(field_name, t), mask)[_idx].to(pt.float32)
 
     # save everything
-    pt.save({field_name: _data, "write_times": _write_times, "xz": _coord},
+    print("Saving data.")
+    pt.save({field_name: _data.to(pt.float32), "write_times": _write_times, "xz": _coord.to(pt.float32)},
             join(save_path, f"{field_name}_fields_{case}.pt"))
+
+
+def compute_aoa(time_steps, aoa0: Union[int, float], amplitude: Union[int, float], frequency: Union[int, float],
+                phase: Union[int, float] = 0) -> pt.Tensor:
+    # verified with actual inflow angle from the simulations (in ParaView)
+    return aoa0 + amplitude * pt.sin(2*pt.pi*frequency*time_steps - pt.deg2rad(pt.tensor(phase)))
 
 
 if __name__ == '__main__':
     # load and save paths
-    """
+    # """
     load_dir = join("/media", "janis", "Elements", "Janis", "2D_buffet_simulation",
                     "URANS_2D_Ma0.73_Re3e6_pitching_volume_data")
     save_dir = join("..", "run", "plots", "URANS_pitching", "URANS_blockMesh", "synchronization_analysis_3.5deg",
                     "animations")
-    case = r"A1.75_f20"
+    case = r"A1.75_f6"
     #"""
 
-    load_dir = join("/media", "janis", "Elements", "Janis", "2D_buffet_simulation", "DDES_3D_Ma0.73_Re3e6")
-    save_dir = join("..", "run", "plots", "DDES_validation", "animations")
-    case = r"DDES_SALSA_Re3e6_Ma0.73_alpha3.5deg_y65_ymax0.25"
+    # load_dir = join("/media", "janis", "Elements", "Janis", "2D_buffet_simulation", "DDES_3D_Ma0.73_Re3e6")
+    # save_dir = join("..", "run", "plots", "DDES_validation", "animations")
+    # case = r"DDES_SALSA_Re3e6_Ma0.73_alpha3.5deg_y65_ymax0.25"
 
     # settings
-    prepare = True
+    prepare = False
     bounds = [[-0.25, -1, -0.25], [5, 1, 2]]
 
+    # if we have pitching, add an arrow with the current AoA
+    pitching = True
+    aoa0 = 3.5
+    frequency = 6
+    amplitude = 1.75
+    t_pitching_start = 0.06
+
     # 2D URANS
-    # n_dims = 2
-    # y_max = -0.0375
-    # t_start = 2
+    n_dims = 2
+    y_max = -0.0375
+    t_start = 2
 
     # 3D DDES
-    y_max = -0.25
-    n_dims = 3          # it is assumed that the y-coord. is the spanwise direction
-    t_start = 0.15
+    # y_max = -0.25
+    # n_dims = 3          # it is assumed that the y-coord. is the spanwise direction
+    # t_start = 0.15
 
     # flow conditions
     chord = 1
@@ -122,11 +136,22 @@ if __name__ == '__main__':
         # load the airfoil coordinates
         oat = read_csv(join("..", "grid_generation", "oat15.dat"), sep=r"\s+", skiprows=1, header=None, names=["x", "y"])
 
+    # if we have pitching, compute the AoA.
+    # note: this is the AoA @ inlet, but in video we don't show the inlet -> there is a delay
+    if pitching:
+        idx_start = pt.where(abs(pt.tensor(forces.t.values) - t_pitching_start) < 1e-8)[0][0].item()
+        aoa = compute_aoa(pt.from_numpy(forces.t.values[idx_start:]), aoa0, amplitude, frequency)
+
     # use latex fonts
     plt.rcParams.update({"text.usetex": True, "figure.dpi": 360})
 
     # set the fps, make sure to not set it to zero if we haven't enough snapshots
     fps = int(len(write_times) / 10) if len(write_times) >= 150 else 15
+
+    # add the arrow
+    if pitching:
+        arrow_origin = pt.tensor([-0.2/chord, 0.0])
+        arrow_length = 0.2
 
     # animate flow field only
     fig, ax = plt.subplots(figsize=(6, 3))
@@ -154,6 +179,16 @@ if __name__ == '__main__':
         ax.set_aspect("equal")
         ax.set_title(fr"$\tau = {write_times[i] * u_inf / chord:.2f}$")
 
+        # add arrow for AoA
+        if pitching:
+            idx = pt.where(abs(pt.tensor(forces.t.values) - write_times[i]) < 1e-6)[0][0].item()
+            dx = arrow_length * pt.cos(pt.deg2rad(aoa[idx - idx_start]))
+            dz = arrow_length * pt.sin(pt.deg2rad(aoa[idx - idx_start]))
+
+            arrow = FancyArrowPatch(posA=(arrow_origin[0], arrow_origin[1]), posB=(arrow_origin[0] + dx, arrow_origin[1] + dz),
+                                    color="black", arrowstyle="->", mutation_scale=12, linewidth=2)
+            ax.add_patch(arrow)
+
         return cf
 
     ax.set_xlim(-0.2/chord, 2.5/chord)
@@ -167,7 +202,10 @@ if __name__ == '__main__':
 
     ani = FuncAnimation(fig, animate, frames=field.shape[1], blit=False, repeat=True)
     writer = FFMpegWriter(fps=fps)
-    ani.save(join(save_dir, f"flow_field_animation_{field_name}_{case}.mp4"), writer=writer)
+    if pitching:
+        ani.save(join(save_dir, f"flow_field_animation_{field_name}_{case}_with_arrow.mp4"), writer=writer)
+    else:
+        ani.save(join(save_dir, f"flow_field_animation_{field_name}_{case}.mp4"), writer=writer)
     plt.close(fig)
 
     #  ----------------------------- animate flow field with cl together -------------------------------------
@@ -238,9 +276,21 @@ if __name__ == '__main__':
         ax[1].set_ylabel(r"$c_l$")
         ax[1].minorticks_on()
 
+        # add arrow for AoA
+        if pitching:
+            idx = pt.where(abs(pt.tensor(forces.t.values) - write_times[i]) < 1e-6)[0][0].item()
+            dx = arrow_length * pt.cos(pt.deg2rad(aoa[idx - idx_start]))
+            dz = arrow_length * pt.sin(pt.deg2rad(aoa[idx - idx_start]))
+
+            arrow = FancyArrowPatch(posA=(arrow_origin[0], arrow_origin[1]), posB=(arrow_origin[0] + dx, arrow_origin[1] + dz),
+                                    color="black", arrowstyle="->", mutation_scale=12, linewidth=2)
+            ax[0].add_patch(arrow)
         return cf
 
     # create animation
     ani = FuncAnimation(fig, animate, frames=field.shape[1], blit=False, repeat=True)
     writer = FFMpegWriter(fps=fps)
-    ani.save(join(save_dir, f"flow_field_cl_animation_{field_name}_{case}.mp4"), writer=writer)
+    if pitching:
+        ani.save(join(save_dir, f"flow_field_cl_animation_{field_name}_{case}_with_arrow.mp4"), writer=writer)
+    else:
+        ani.save(join(save_dir, f"flow_field_cl_animation_{field_name}_{case}.mp4"), writer=writer)
